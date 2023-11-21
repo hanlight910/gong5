4d4d644200bce6b234f8a7e1a0243e4a7b023e15
harley <leehanvit511@gmail.com>
2023-11-21 21:21:02 +0900

    add: [auth] comment, product, userModel.js

---
+++ models/productModels.js
@@ -0,0 +1,52 @@
+const { DataTypes }  = require('sequelize');
+const db = require('../config/database');
+const Auth = require("./userModel");
+const User = db.define('Product', {
+    id : {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement : true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    birthday : {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    createdtime: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    updatetime: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    profileImage: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+});
+
+module.exports = Product;
