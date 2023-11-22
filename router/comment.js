9e5b8235631d2c9978e02dc430605051bede0e62
Hong <qs1225qs@naver.com>
2023-11-22 23:22:48 +0900

    24번 수정중, pull할거 있어서 커밋함

--- router/comment.js
+++ router/comment.js
@@ -8,23 +8,27 @@ const ProductInfo = require('../models/productInfo');
 
 router.post('/comment/:product_id', async (req, res) => { // API comment/:product_id(제품의 PK값)이 필요하지 않은지??
   try {
-    // 댓글을 달 제품의 아이디 값
-    const product_id = req.params.product_id;
-
     // 전달해줄 댓글 내용
     const { comment } = req.body;
+    
+    // 댓글을 달 제품의 아이디 값
+    const product_id = req.params.product_id;
 
     // 댓글을 단 제품의 아이디, 전달받은 params값과 일치하는 아이디값을 찾는다.
     const productId = await ProductInfo.findOne({ where: { id : product_id }});
 
     // 댓글을 단 유저의 아이디
-    const userId = await UserInfo.id;
-
+    // const userId = await UserInfo.findOne({ id: userid })
+    // console.log("userId => ", userId)
     // db에 comment, 댓글단 유저의 아이디, 제품아이디를 저장한다.
-    const addComment = await CommentInfo.create({ comment, product_id: productId, user_id: userId });
+    const addComment = await CommentInfo.create({ 
+      comment, 
+      product_id: productId, 
+      user_id: 1 
+    });
 
 
-    res.status(201).json({ message: '댓글 등록 완료'}); // 썬더에서 보내지는거 확인
+    res.status(201).json({ message: '댓글 등록 완료', });
 
   } catch (error) {
     console.error(error);
