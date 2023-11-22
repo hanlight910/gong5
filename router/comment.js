1ff213bc58cfeaab737eddbdf13dcdfb2ad040da
Hong <qs1225qs@naver.com>
2023-11-22 19:17:05 +0900

    생성 API 작성, 24번줄 저장하는 부분에서 에러 찾는중

--- router/comment.js
+++ router/comment.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-const Comment = require('../models/commentModel');
+const CommentInfo = require('../models/commentInfo');
+const UserInfo = require('../models/userInfo');
+const ProductInfo = require('../models/productInfo');
 
-router.post('/comments', async (req, res) => {
+
+router.post('/comment/:product_id', async (req, res) => { // API comment/:product_id(제품의 PK값)이 필요하지 않은지??
   try {
+    // 댓글을 달 제품의 아이디 값
+    const product_id = req.params.product_id;
+
     // 전달해줄 댓글 내용
-    const { content } = req.body;
+    const { comment } = req.body;
 
-    // db에 content, name, createAt를 저장한다.
-    const addComment = await Comment.create({
-      content, // 작성한 댓글
-      name, // 댓글단 사용자의 이름
-    });
+    // 댓글을 단 제품의 아이디, 전달받은 params값과 일치하는 아이디값을 찾는다.
+    const productId = await ProductInfo.findOne({ where: { id : product_id }});
 
-    const responseContent = {
-      name: addComment.name,
-      content: addComment.content,
-    }
+    // 댓글을 단 유저의 아이디
+    const userId = await UserInfo.id;
 
-    // 저장이 완료되면 댓글등록완료 메세지와, db에 저장된 작성자명,댓글내용을 보여준다.
-    res.status(201).json({ message: '댓글 등록 완료', comment: addComment });
+    // db에 comment, 댓글단 유저의 아이디, 제품아이디를 저장한다.
+    const addComment = await CommentInfo.create({ comment, product_id: productId, user_id: userId });
+
+
+    res.status(201).json({ message: '댓글 등록 완료'}); // 썬더에서 보내지는거 확인
 
   } catch (error) {
     console.error(error);
