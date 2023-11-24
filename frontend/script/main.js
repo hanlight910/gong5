f6d53070e0a4f448ed5a34bff298d3d1e20089a9
harley <leehanvit511@gmail.com>
2023-11-25 02:55:14 +0900

    feat: [댓글-수정] main 불러오는 건 성공, 하지만 html 추가 안됨

--- frontend/script/main.js
+++ frontend/script/main.js
@@ -1,22 +1,33 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    // 서버 API 엔드포인트
-    const apiUrl = 'http://localhost:3010/products';
-
-    // fetch를 사용하여 GET 요청 보내기
-    fetch(apiUrl)
-        .then(response => response.json())
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to fetch products from the backend API
+    function fetchProducts(sort) {
+      // Make an AJAX request to the backend API
+      fetch(`http://localhost:3010/products`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
         .then(data => {
-            // 서버에서 받아온 중고 제품 목록을 화면에 표시
-            const productList = document.getElementById('productList');
-            data.products.forEach(product => {
-                const listItem = document.createElement('li');
-                listItem.textContent = `제목: ${product.title}, 가격: ${product.price}, 판매자: ${product.userInfo.name}`;
-                productList.appendChild(listItem);
-            });
+          // Handle the successful response from the server
+          console.log(data.products); // Log the products to the console (for testing)
+        
         })
         .catch(error => {
-            console.error('에러 발생:', error);
-            // 에러 처리 작업 수행
+          console.error('Error fetching products:', error);
         });
+    }
+
+    
+    fetchProducts('DESC'); 
+
+    
+    const sortButton = document.getElementById('sortButton');
+    if (sortButton) {
+      sortButton.addEventListener('click', function () {
+        const sortOrder = 'ASC';
+        fetchProducts(sortOrder);
+      });
+    }
 });
